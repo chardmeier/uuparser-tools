4d9f9ba0e7a9d518873cb9cace2cf07e54fca258
Nyx <Nyxjemk@gmail.com>
2019-07-18 13:49:06 +0200

    uncomment print

--- scripts/test.py
+++ scripts/test.py
@@ -5,12 +5,12 @@ d = {}
 # traverse root directory, and list directories as dirs and files as files
 for root, dirs, files in os.walk("ud-treebanks-v2.4"):
     path = root.split(os.sep)
-    #print((len(path) - 1) * '---', os.path.basename(root))
+    print((len(path) - 1) * '---', os.path.basename(root))
     curr_dir = os.path.basename(root)
     for file in files:
         if file.endswith('ud-train.conllu'):
             d[curr_dir] = file.split('-')[0]
-            #print(len(path) * '---', file)
+            print(len(path) * '---', file)
 	
 import json
 
