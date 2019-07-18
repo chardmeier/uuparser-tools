251336c9dcbeb32021a19b9922abc08476dbac15
Nyx <Nyxjemk@gmail.com>
2019-07-18 23:13:12 +0200

    modify test.py

--- scripts/test.py
+++ scripts/test.py
@@ -5,12 +5,13 @@ d = {}
 lang2code = {}
 
 parser = argparse.ArgumentParser(description='Extracts language to code mappings from ud-treebanks')
-parser.add_argugment('treebank_dir', type=str, help='path to the ud-treebank-directory')
-parser.add_argugment('--output', '-o', type=str, default='lang2code.dict', help='specifies optional output path (default="lang2code.dict")')
+parser.add_argument('treebank_dir', type=str, help='path to the ud-treebank-directory')
+parser.add_argument('--output', '-o', type=str, default='lang2code.dict', help='specifies optional output path (default="lang2code.dict")')
 args = parser.parse_args()
 
 assert os.path.isdir(args.treebank_dir)
 count = 0
+print('Reading directory..')
 # traverse root directory, and list directories as dirs and files as files
 for root, dirs, files in os.walk(args.treebank_dir):
     path = root.split(os.sep)
@@ -30,6 +31,7 @@ import json
 #with open('ud2.4_iso.json', 'w') as json_file:
 #    json.dump(d, json_file)
 
-
+print(f'{count} valid treebanks found.')
+print(f'Writing output to: "{args.output}"')
 with open(args.output, 'w') as f:
     f.write(repr(lang2code))
