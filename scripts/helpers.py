d789ad0c2561f93edab1fa46339f21d9cf36ad55
Nyx <Nyxjemk@gmail.com>
2019-07-19 21:24:35 +0200

    add debug print

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -3,13 +3,13 @@ from .config import SCRIPTS, MODELS, TOKENIZER_NAME
 
 
 def select_treebank(lang, code2lang, model_dir):
-    print(model_dir, lang)
+    #print(model_dir, lang)
     l = os.listdir(model_dir)
-    select = list(filter(lambda key: f'{lang}_' in key, code2lang))
-    print(select)
+    select = list(filter(lambda key: f'{lang}_' in key, code2lang)) # select all keys for this language in code2lang.dict
+    #print(select)
     assert bool(select), f'No models found for for language {lang}'
-    possible_treebanks = list(map(code2lang.get, select))
-    print(possible_treebanks)
+    possible_treebanks = list(map(code2lang.get, select))           # convert lang short cut to language file name as used by udpipe pre-trained models
+    #print(possible_treebanks)
     reg_term = f'{possible_treebanks[0].split("-")[0].lower()}*'
     selected = os.popen(f'du -sh {os.path.join(model_dir, reg_term)}').read().split()[1]
     return os.path.abspath(selected)
--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -17,6 +17,7 @@ def tokenize(arg1, model_path=None):
     print(f'Tokenized file will be saved to:')
     
     lang = input_filename.split('.')[-1]  
+    assert len(lang) == 2, 'Language detection is currently only working with binary language codes such as "de"'
     model_path = default_by_lang(lang)
     print(f'Using UDPipe model: {model_path}')
     """if not model_path:
