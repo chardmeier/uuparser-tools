04d073b977b314a54b6b1c0b97accb1c185d7bd7
Nyx <Nyxjemk@gmail.com>
2019-07-19 19:27:21 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+DATA   = os.environ.get('DATA')
+MODELS = os.environ.get('MODELS')
+PROJECT = os.environ.get('PROJECT')
+BATCHFILES = os.environ.get('BATCHFILES')
+LOGS = os.environ.get('LOGS')
+SCRIPTS = os.environ.get('SCRIPTS')
+PARSER = os.environ.get('PARSER')
+
+TOKENIZER_NAME = os.environ.get('NAME_TOKENIZER')
+
+MODELS = "abs/models"
+DATA = 'abc/data'
+PARSER_NAME = 'UUParser'
+PARSER = "abs/uuparser/barchybrid"
+LOGS = "abs/logfiles"
