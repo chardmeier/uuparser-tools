ab4a06bf86f79c6fe508ec7b412dbf387bbac7f1
Nyx <Nyxjemk@gmail.com>
2019-07-19 21:01:53 +0200

    alpha version of command line tokenize with pre-trained models

---
+++ scripts/helpers.py
@@ -0,0 +1,23 @@
+import os, sys
+from config import SCRIPTS, MODELS, TOKENIZER_NAME
+
+
+def select_treebank(lang, code2lang, model_dir):
+    l = os.listdir(model_dir)
+    select = list(filter(lambda key: f'{lang}_' in key, code2lang))
+    possible_treebanks = list(map(code2lang.get, select))
+    reg_term = f'{possible_treebanks[0].split("-")[0].lower()}*'
+    selected = os.popen(f'du -sh {os.path.join(model_dir, reg_term)}').read().split()[1]
+    return os.path.abspath(selected)
+    
+def default_by_lang(lang)
+    tokenizer_model_dir = os.path.join(MODELS, TOKENIZER_NAME)
+    with open(os.path.join(SCRIPTS, 'code2lang.dict')) as f:
+        code2lang = eval(f.read())
+    return select_treebank(lang, code2lang, tokenizer_model_dir)
+
+
+def create_dir(dir_path):
+    if not os.path.isdir(dir_path):
+        print('Create directory:', dir_path)
+        os.mkdir(dir_path)
---
+++ tool.py
@@ -0,0 +1,41 @@
+import argparse
+
+batch_job_options  = argparse.ArgumentParser(add_help=False)
+batch_group = batch_job_options.add_argument_group('Batch Job Script options')
+batch_group.add_argument('--mem', '-m', default='60GB', type=str, help='Sets amount of memory allocated for the job. For example "60GB"')
+batch_group.add_argument('--duration', '-d', default='96:00:00', type=str, help='Sets the TimeLimit for the job. For example "96:00:00"')
+batch_group.add_argument('--name', '-n', type=str, help='Option for setting a manual name for batch job.')
+batch_group.add_argument('--partition', type=str, choices=['normal', 'hugemem', 'accel'], default='normal', help='Setting partition for the job to run on (default=normal).')
+
+
+
+arg_parser = argparse.ArgumentParser()
+
+subparsers = arg_parser.add_subparsers(title="commands", dest="command", help='Tool options:')
+
+# A list command
+sub_name = 'parser'
+uuparser = subparsers.add_parser(sub_name, help='Options for UUParser', parents=[batch_job_options])
+train_parse = uuparser.add_mutually_exclusive_group(required=True)
+train_parse.add_argument('--parse', '-p', type=str, help='Expects path to .conll file that will be parsed using UUParser')
+train_parse.add_argument('--train', '-t', type=str, help='Trains UUParser on a given treebank, expects treebank language code as input such as "de_gsd"')
+
+# A create command
+sub_name = 'tokenizer'
+tokenizer = subparsers.add_parser(sub_name, help='Options for UDPipe Tokenizer', parents=[batch_job_options])
+train_tokenize = tokenizer.add_mutually_exclusive_group(required=True)
+train_tokenize.add_argument('--tokenize', type=str, help='Expects path to .txt file that will be tokenized using UDPipe')
+train_tokenize.add_argument('--train', '-t', type=str, help='Trains UDPipe on a given treebank, expects treebank language code as input such as "de_gsd"')
+train_tokenize.add_argument('--custom_model', '-c', action='store_true', help='If set UDPipe will load custom models instead of the official pre-trained ones.')
+
+# A delete command
+sub_name = 'align'
+eflomal = subparsers.add_parser(sub_name, help='Options for Eflomal alignment tool', parents=[batch_job_options])
+eflomal.add_argument('input', action='store', help='Input for eflomal')
+#delete_parser.add_argument('--file', '-r', default=False, action='store_true',
+#                           help='Remove the contents of the directory, too',
+#                           )
+args = arg_parser.parse_args()
+print (args)
+#print (parser.parse_args())
+
