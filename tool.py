7ed2557c88f3b2bf664a36397f1bc689311c5fb3
Nyx <Nyxjemk@gmail.com>
2019-07-19 21:05:37 +0200

    adding commands in tool.py

--- tool.py
+++ tool.py
@@ -15,27 +15,35 @@ subparsers = arg_parser.add_subparsers(title="commands", dest="command", help='T
 
 # A list command
 sub_name = 'parser'
-uuparser = subparsers.add_parser(sub_name, help='Options for UUParser', parents=[batch_job_options])
-train_parse = uuparser.add_mutually_exclusive_group(required=True)
+uuparser_args = subparsers.add_parser(sub_name, help='Options for UUParser', parents=[batch_job_options])
+train_parse = uuparser_args.add_mutually_exclusive_group(required=True)
 train_parse.add_argument('--parse', '-p', type=str, help='Expects path to .conll file that will be parsed using UUParser')
 train_parse.add_argument('--train', '-t', type=str, help='Trains UUParser on a given treebank, expects treebank language code as input such as "de_gsd"')
 
 # A create command
 sub_name = 'tokenizer'
-tokenizer = subparsers.add_parser(sub_name, help='Options for UDPipe Tokenizer', parents=[batch_job_options])
-train_tokenize = tokenizer.add_mutually_exclusive_group(required=True)
+tokenizer_args = subparsers.add_parser(sub_name, help='Options for UDPipe Tokenizer', parents=[batch_job_options])
+train_tokenize = tokenizer_args.add_mutually_exclusive_group(required=True)
 train_tokenize.add_argument('--tokenize', type=str, help='Expects path to .txt file that will be tokenized using UDPipe')
 train_tokenize.add_argument('--train', '-t', type=str, help='Trains UDPipe on a given treebank, expects treebank language code as input such as "de_gsd"')
 train_tokenize.add_argument('--custom_model', '-c', action='store_true', help='If set UDPipe will load custom models instead of the official pre-trained ones.')
 
 # A delete command
 sub_name = 'align'
-eflomal = subparsers.add_parser(sub_name, help='Options for Eflomal alignment tool', parents=[batch_job_options])
-eflomal.add_argument('input', action='store', help='Input for eflomal')
+eflomal_args = subparsers.add_parser(sub_name, help='Options for Eflomal alignment tool', parents=[batch_job_options])
+eflomal_args.add_argument('input', action='store', help='Input for eflomal')
 #delete_parser.add_argument('--file', '-r', default=False, action='store_true',
 #                           help='Remove the contents of the directory, too',
 #                           )
 args = arg_parser.parse_args()
 print (args)
 #print (parser.parse_args())
+
+from scripts import tokenizer
+
+if args.command == 'tokenizer':
+    if args.tokenize:
+        tokenizer.tokenize(args.tokenize)
+
+
 
