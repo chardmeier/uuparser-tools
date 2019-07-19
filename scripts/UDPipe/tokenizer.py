bbcabbd791d8f5630d818987305ead87b5f3ee13
Nyx <Nyxjemk@gmail.com>
2019-07-19 21:09:25 +0200

    adding commands in tool.py

--- scripts/UDPipe/tokenizer.py
+++ scripts/UDPipe/tokenizer.py
@@ -1,9 +1,15 @@
 import sys, os, ntpath
 # UDPipe tokenize.py
-from helpers import default_by_lang, create_dir
-from config import *
+
 
 def tokenize(arg1, model_path=None):
+    DATA   = os.environ['DATA']
+    MODELS = os.environ['MODELS']
+    PROJECT = os.environ['PROJECT']
+    BATCHFILES = os.environ['BATCHFILES']
+    LOGS = os.environ['LOGS']
+    SCRIPTS = os.environ['SCRIPTS']
+
     # arg1: path to file that will be tokenized/tagged
     # input file is expected to end with the respective language for example: abc.xy.en
     input_path = os.path.abspath(arg1)
@@ -12,27 +18,30 @@ def tokenize(arg1, model_path=None):
     output_dir  = os.path.join(input_dir, 'conll')
     output_file = os.path.join(output_dir, f'{input_filename}.conll')
 
-    create_dir(output_dir)
+    if not os.path.isdir(output_dir):
+        print('Create directory:', output_dir)
+        os.mkdir(output_dir)
     
-    lang = input_filename.split('.')[-1]  
-    model_path = default_by_lang(lang)  
-    """if not model_path:
-                    model_path = f"{MODELS}/ud_custom_models"
+    lang = input_filename.split('.')[-1]    
+    if not model_path:
+        model_path = f"{MODELS}/ud_custom_models"
+
+        if not os.path.isdir(model_path):
+            print('Create directory:', model_path)
+            os.mkdir(model_path)
             
-                    if not os.path.isdir(model_path):
-                        print('Create directory:', model_path)
-                        os.mkdir(model_path)
-                        
-                    print('Output directory:', output_dir)
-            
-                    sys.path.append(SCRIPTS)
-                    from default_code_mappings import default_mappings as d
-                    #d = {'de':'de_gsd', 'en':'en_ewt', 'cs':'cs_pdt', 'fr':'fr_ftb', 'sv':'sv_talbanken'}
-                    #cs_pdt.model  de_gsd.model  en_ewt.model  fr_ftb.model sv_talbanken.model
-                    model_path = f'{model_path}/{d[lang]}.model'"""
+        print('Output directory:', output_dir)
+
+        sys.path.append(SCRIPTS)
+        from default_code_mappings import default_mappings as d
+        #d = {'de':'de_gsd', 'en':'en_ewt', 'cs':'cs_pdt', 'fr':'fr_ftb', 'sv':'sv_talbanken'}
+        #cs_pdt.model  de_gsd.model  en_ewt.model  fr_ftb.model sv_talbanken.model
+        model_path = f'{model_path}/{d[lang]}.model'
 
     log_path = f"{LOGS}/UDPipe"
-    create_dir(log_path)
+    if not os.path.isdir(log_path):
+        print('Create directory:', log_path)
+        os.mkdir(log_path)
 
     batch_string = f"""#!/bin/sh
 
