4980ef8d6afca9025b73b3439ef639309fc97c27
Nyx <Nyxjemk@gmail.com>
2019-08-18 11:11:11 +0200

    correcting extrac_tokens

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -4,6 +4,30 @@ from .helpers import default_by_lang, create_dir, udpipe_model_to_code
 from .config import *
 from .utils import Batch
 
+
+def replace_chars(target_dir):
+    # Script replaces characters in all files in the respective target directory
+    assert os.path.isdir(target_dir)
+    target_dir = os.path.abspath(target_dir)
+    replace_pairs = [('\u2028', ' '), ('\x85', '')]
+
+    files = os.listdir(target_dir)
+    files = list(filter(lambda x: not os.path.isdir(os.path.join(target_dir, x)), files))
+    print(f'Processing directory: {target_dir} ..')
+    for file in files:
+        print(f' - File: {file}')
+        file = os.path.join(target_dir, file)
+        with open(file) as f:
+            data = f.read()
+            for pair in replace_pairs:
+                data = data.replace(*pair)
+
+        with open(file, 'w') as f:
+            f.write(data)
+    print('Replaced:', replace_pairs)
+    print('For files in:', target_dir)
+
+
 def remove_n(input_file):
     """Removes added \n from file. Note: File will be overwritten!"""
     input_path = os.path.abspath(input_file)
--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -39,41 +39,51 @@ def tokenize(input_file, model_path=None):
     batch.tokenize(model_path=model_path, input_path=input_path, output_file=output_file)
     batch.submit()
 
-def extract_tokens(arg1, new_par_nl=True):
-    # Script takes conll file output by udpipe and extracts the tokenized tokens.
-    # Newline will only be added if SpacesAfter is '\n'
-    input_path = os.path.abspath(arg1)
-    print(f'Reading file: {input_path}')
-    input_filename = ntpath.basename(input_path)
-    input_dir = os.path.dirname(input_path)
-    output_dir  = os.path.join(input_dir, 'tokens')
+def extract_tokens(arg1):
+    #main_dir  = os.path.abspath(sys.argv[1])
+
+    # set ending of output files  (dot (.) must be included !):
+    ending = '' # '.token'
+
+    #input_dir = os.path.join(main_dir, 'conll')
+    input_dir = os.path.abspath(arg1)
+    assert os.path.isdir(input_dir)
+
+    output_dir  = os.path.join(main_dir, 'tokens')
+
     create_dir(output_dir)
-    print(f'Tokens will be saved to:', output_dir)
 
-    assert input_filename.endswith('.conll')
-    output_file  = '.'.join(file.split('.')[:-1] + ['txt']) 
-    output_path  = os.path.join(output_dir, output_file)
+    print('Loading directory: ', input_dir)
 
-    with open(file) as f:
-        print('Opening file:', file)
-        lines = []
-        line  = []
-        for token_line in f:
-            if (not token_line.startswith('#')) and (token_line != '\n'): # ignores newlines and those starting with #
-                token_line = token_line.split('\t')
-                line_no, token = token_line[0], token_line[1] # extract token
-                if not ('-' in line_no):
-                    line.append(token_line[1])                # adding token to current line
-
-                if token_line[9].split('=')[-1] == '\\n\n':   # checks for newline
-                    lines.append(' '.join(line) + '\n')       # joins tokens with newline at the end
-                    line = []                                 # initialize new line
-            elif new_par_nl and token_line.startswith('# newpar'):
-                lines.append(['\n'])
-    
-    with open(output_path, 'w') as f:
-        print('Writing tokens to:', output_path)
-        f.writelines(lines)
+    files = list(filter(lambda f: f.endswith('.conll'), os.listdir(input_dir)))
+    print('Found .conll files:',)
+    pprint.pprint(files)
+    print()
+    print('Output directory:', output_dir)
+    print()
+    for file in files:
+        in_path = os.path.join(input_dir, file)
+        with open(in_path) as f:
+            print('Reading file:', in_path)
+            lines = []
+            line  = []
+            for token_line in f:
+                if (not token_line.startswith('#')) and (token_line != '\n'):
+                    token_line = token_line.split('\t')
+                    line_no, token = token_line[0], token_line[1] # extract token
+                    if not ('-' in line_no):
+                        line.append(token_line[1])  
+                    n = re.findall(r'\\n', token_line[9])
+                    if n:
+                        lines.append(' '.join(line) + '\n'*len(n))
+                        line = []
+                        
+        out_file = '.'.join(file.split('.')[:-1]) + ending
+        out_path = os.path.join(output_dir, out_file)
+        with open(out_path, 'w') as f:
+            print(f' \u2b91  writing tokens ({len(lines)} lines) to:', out_path)
+            f.writelines(lines)
+        print()
 
 if __name__ == '__main__':
     tokenize(sys.argv[1])
