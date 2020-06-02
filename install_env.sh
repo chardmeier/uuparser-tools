89825dad00e6d8fd86802637bb3a615354b1ec1a
Christian Hardmeier <chriha@b-an01.hpc2n.umu.se>
2020-06-02 18:16:58 +0200

    Changes to install_env.sh.

--- install_env.sh
+++ install_env.sh
@@ -1,25 +1,20 @@
+#! /bin/bash
 
+set -e
 
-#wget https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
-#bash Miniconda2-latest-Linux-x86_64.sh -b -p miniconda
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda
-
-rm Miniconda3-latest-Linux-x86_64.sh
+. "/pfs/nobackup/home/c/chriha/miniconda3/etc/profile.d/conda.sh"
 
 MAIN="$(pwd)"
 
-mkdir software
+mkdir -p software
 SOFTWARE="$(pwd)/software"
 
-mkdir $SOFTWARE/bin
+mkdir -p $SOFTWARE/bin
 BIN_DIR="$SOFTWARE/bin"
 
 echo "Writing into pn.env"
 
-echo ". $(pwd)/miniconda/etc/profile.d/conda.sh" >> pn.env
-echo "conda activate base" >> pn.env
+echo ". $HOME/pfs/miniconda3/bin/activate uuparser" >> pn.env
 echo "export PROJECT=$(pwd)" >> pn.env
 echo "" >> pn.env
 echo "module load GCC " >> pn.env
@@ -28,7 +23,8 @@ echo "export BIN_DIR=$BIN_DIR" >> pn.env
 echo "export PATH=$BIN_DIR:\$PATH" >> pn.env
 #echo "module load python3/3.7.0.gnu"
 
-. miniconda/etc/profile.d/conda.sh
+# . $HOME/pfs/miniconda3/etc/profile.d/conda.sh
+conda create -n uuparser python=3 numpy==1.16.1 cython
 
 cd $SOFTWARE
 
@@ -38,7 +34,8 @@ rm udpipe-1.2.0-bin.zip
 mv $SOFTWARE/udpipe-1.2.0-bin/bin-linux64/udpipe $BIN_DIR/
 
 
-conda activate base
+conda activate uuparser
+conda install cython
 #pip install dynet
 
 git clone https://github.com/maxtrem/uuparser
@@ -58,4 +55,5 @@ python setup.py install
 
 
 
-echo "Envoroment installation script finished. Use 'source pn.env' to activate."
+echo "Environment installation script finished. Use 'source pn.env' to activate."
+
